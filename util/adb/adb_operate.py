e60dd98c94ca48b3455c7c817281185bd0a4594b
song <songyw9812@gmail.com>
2019-08-31 15:37:16 +0900

    refactioring adb

---
+++ util/adb/adb_operate.py
@@ -0,0 +1,16 @@
+from util.initial import client
+from util.adb.connection import *
+from pathlib import Path
+
+
+def adb_operation(operate):
+    if operate == 'pull':
+        return
+
+
+def adb_pull(device, path):
+    _path = Path(path)
+    path_part = _path.parts
+    file = Path() / path_part[-2] / path_part[-1]
+
+    device.pull(_path.parent, './backup/' + str(file))
---
+++ util/adb/connection.py
@@ -0,0 +1,44 @@
+from util.initial import client
+from util.noti import err_printer, info_printer
+from util.tool import input_int
+
+
+def package_list(connect):
+    packages = connect.socket.makefile()
+    data = packages.readlines()
+
+
+def check():
+    list_device = client.devices()
+
+    if len(list_device) is 0:
+        err_printer('not_connected')
+
+        while len(client.devices()) is 0:
+            pass
+        info_printer('connected')
+        list_device = client.devices()
+
+    if len(list_device) > 1:
+        info_printer('one_more_con')
+        for i, d in enumerate(list_device):
+            print('{0}. {1}'.format(i, d.serial))
+        i = input_int()
+        return list_device[i]
+
+    return list_device[0]
+
+
+def is_valid(user):
+    connected_list = []
+
+    for i in client.devices():
+        connected_list.append(i.serial)
+
+    if user.serial not in connected_list:
+        err_printer('disconnected')
+
+        while user not in client.devices():
+            pass
+        info_printer('connected')
+    return user
