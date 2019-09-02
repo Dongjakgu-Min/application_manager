6afc285daed44baa8c8d1743ac2a5f73ea5fea07
song <songyw9812@gmail.com>
2019-09-02 17:58:06 +0900

    fix cannot change input mode

--- util/adb/adb_operate.py
+++ util/adb/adb_operate.py
@@ -11,6 +11,7 @@ def adb_operation(operate):
 def adb_pull(device, path):
     _path = Path(path)
     path_part = _path.parts
+    print(path_part)
     file = Path() / path_part[-2] / path_part[-1]
 
     device.pull(_path.parent, './backup/' + str(file))
